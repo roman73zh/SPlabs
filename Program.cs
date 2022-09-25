dd12d5fd08ddf92f7b057b96c5f0db69e6b2701a
Roman <zadligo4@mail.ru>
2022-09-25 20:28:29 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Lab1;
+Console.WriteLine("Hello, World!");
+LexemProc proc = new LexemProc();
+proc.LoadFile("../../../input.txt");
+proc.Process();
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\nLexems:");
+foreach(var lexem in proc.GetLexems()) 
+{
+    Console.WriteLine(lexem.ToString());
+}
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\nVariables:");
+foreach (var variable in proc.GetVariables())
+{
+    Console.WriteLine(variable.ToString());
+}
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\nIdentifiers:");
+foreach (var identifier in proc.GetIdentifiers())
+{
+    Console.WriteLine(identifier.ToString());
+}
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
